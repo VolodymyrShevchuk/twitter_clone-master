259e9274dcdb302410f6556eab5de1169533747f
VovaShevchuk <v.shevchuk@chnu.edu.ua>
2018-05-16 17:01:49 +0300

    Created authenticate

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
+  get 'log_in' => 'sessions#new'
+  delete 'log_out' => 'sessions#destroy'
+  get 'sign_up' =>'users#new'
+
+
+
+  resources :users
+  resources :sessions
+  resources :home
+
+
   root 'home#index'
+
 end
