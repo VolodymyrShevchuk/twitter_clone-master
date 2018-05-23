2ad91cb430eeaab9c9e7a44017ef936af5fa1794
WoldemarSh <v.shevchuk@chnu.edu.ua>
2018-05-23 17:05:43 +0300

    normalno

--- db/migrate/20180518172331_create_relationships.rb
+++ db/migrate/20180518172331_create_relationships.rb
@@ -4,13 +4,10 @@ class CreateRelationships < ActiveRecord::Migration[5.1]
 
       t.references :follower
       t.references :followed
-
       t.timestamps
     end
 
-    add_index :relationships, :follower
-    add_index :relationships, :followed
-    add_index :relationships, [:followed, :follower], unique: true
+    add_index :relationships, [:followed_id, :follower_id], unique: true
 
   end
 end
